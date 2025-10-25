307612bf7d8a983187e9aa242a2a479469474f68
Ahmed Abdulrahim <hemo089@outlook.com>
2025-10-25 14:49:44 +0300

    Add project files.

---
+++ FoodPL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FoodPL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
